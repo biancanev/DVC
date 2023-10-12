7963dbb5947a075e506e4cd5f022eb269afa17fa
Ryan Kwong <bebostunner234@gmail.com>
2023-10-11 19:44:35 -0700

    Update LabExercise8.asm

--- COMSC_260/Lab Exercises/LabExercise8.asm
+++ COMSC_260/Lab Exercises/LabExercise8.asm
@@ -1,4 +1,4 @@
-; Assignment 7
+; Lab Exercise 8
 ; Programmer Name: Ryan Kwong(1440216)
 
 INCLUDE Irvine32.inc
@@ -14,27 +14,33 @@ prompt3 byte "The sum is: ", 0
 main PROC
 	mov ecx, 3
 L1:
+	call Randomize
 	mov eax, 256
 	call RandomRange
 	call SetTextColor
 	call Clrscr 
 	call GetMaxXY
 	mov dl, 40
-	mov dh, 15
+	mov dh, 10
 	call Gotoxy
 	mov edx, OFFSET prompt1
 	call WriteString
 	call ReadInt
-	call Crlf
+	mov dl, 40
+	mov dh, 11
+	call Gotoxy
 	mov ebx, eax
 	mov edx, OFFSET prompt2
 	call WriteString
 	call ReadInt
-	call Crlf
+	mov dl, 40
+	mov dh, 12
+	call Gotoxy
 	add eax, ebx
 	mov edx, OFFSET prompt3
 	call WriteString
 	call WriteInt
+	call Crlf
 	call WaitMsg
 	loop L1
 	invoke ExitProcess,0		;end program
